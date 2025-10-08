ec981fc5c5a2d9b264fac5c35f9e54bab8e88328
cosminbrn <cosminbaroana06@gmail.com>
2025-10-08 21:29:33 +0300

    Draw functions for all the tetromino

--- src/brick_interface/bricktetris/constants.py
+++ src/brick_interface/bricktetris/constants.py
@@ -23,14 +23,28 @@ SHAPES = {
           [1, 1, 1]]
 }
 
+# colors
+GRAY = (30, 30, 30)
+
+LIGHT_BLUE = (137, 207, 240)
+DARK_BLUE = (0, 0, 255)
+WHITE = (255, 255, 255)
+CYAN = (0, 255, 255)
+YELLOW = (255, 255, 0)
+PURPLE = (128, 0, 128)
+GREEN = (0, 255, 0)
+RED = (255, 0, 0)
+BLUE = (0, 0, 255)
+ORANGE = (255, 165, 0)
+
 SHAPE_COLORS = {
-    'I': (0, 255, 255), # cyan
-    'O': (255, 255, 0), # yellow
-    'T': (128, 0, 128), # purple
-    'S': (0, 255, 0), # green
-    'Z': (255, 0, 0), # red
-    'J': (0, 0, 255), # blue
-    'L': (255, 165, 0) # orange
+    'I': CYAN ,
+    'O': YELLOW,
+    'T': PURPLE,
+    'S': GREEN,
+    'Z': RED,
+    'J': BLUE,
+    'L': ORANGE
 }
 
 # screen positioning
@@ -55,11 +69,6 @@ MAIN_SCREEN_X = SCREEN_WIDTH // 3
 INFO_SCREEN_X = LEFT_RIGHT_MARGIN
 NEXT_SCREEN_X = SCREEN_WIDTH - LEFT_RIGHT_MARGIN - NEXT_SCREEN_WIDTH
 
-# colors
-LIGHT_BLUE = (137, 207, 240)
-DARK_BLUE = (0, 0, 255)
-WHITE = (255, 255, 255)
-
 # info screen positioning
 TOP_TEXT_Y_OFFSET = 25
 TOP_TEXT_X_OFFSET = INFO_SCREEN_WIDTH // 2 - 40
@@ -78,5 +87,9 @@ BOTTOM_INFO_X_OFFSET = INFO_SCREEN_WIDTH // 2 - 10
 TEXT_Y_OFFSET = 37
 TEXT_X_OFFSET = NEXT_SCREEN_WIDTH // 2 - 40
 
-# tetromino 
-TETROMINO_SIZE = 50
+# tetromino
+TS = 50 # TETROMINO_SIZE
+TBS = 4 # TETROMINO_BORDER_SIZE
+TBSH = TBS // 2 # TETROMINO_BORDER_SIZE_HALF
+TETROMINO_START_Y = MAIN_SCREEN_TOP_DOWN_MARGIN + MAIN_SCREEN_TOP_DOWN_BORDER_SIZE
+TETROMINO_START_X = MAIN_SCREEN_X + 70 + 4 * TS
--- src/brick_interface/bricktetris/game.py
+++ src/brick_interface/bricktetris/game.py
@@ -1,7 +1,7 @@
 import pygame
 from .constants import *
 from .screens import *
-
+from .tetromino import Tetromino
 class Bricktetris:
     name = "Brick Tetris"
     running = True
@@ -16,6 +16,15 @@ class Bricktetris:
         # screens
         self.info_screen = InfoScreen(self.screen, INFO_SCREEN_X, INFO_SCREEN_Y, self.info_screen_image, 0, 1, 0)
         self.next_screen = NextScreen(self.screen, NEXT_SCREEN_X, NEXT_SCREEN_Y, self.next_screen_image, None)
+        self.tetromino_t = Tetromino(self.screen, TETROMINO_START_X - 2 * TS, TETROMINO_START_Y + TS, 'T')
+        self.tetromino_i = Tetromino(self.screen, TETROMINO_START_X - 2 * TS, TETROMINO_START_Y + 3 * TS, 'I')
+        self.tetromino_o = Tetromino(self.screen, TETROMINO_START_X - 1 * TS, TETROMINO_START_Y + 3 * TS, 'O')
+        self.tetromino_z = Tetromino(self.screen, TETROMINO_START_X - 1 * TS, TETROMINO_START_Y + TS, 'Z')
+        self.tetromino_j = Tetromino(self.screen, TETROMINO_START_X + 2 * TS, TETROMINO_START_Y + 5 * TS, 'J')
+        self.tetromino_l = Tetromino(self.screen, TETROMINO_START_X + 2 * TS, TETROMINO_START_Y + 15 * TS, 'L')
+        self.tetromino_s = Tetromino(self.screen, TETROMINO_START_X + 2 * TS, TETROMINO_START_Y + 10 * TS, 'S')
+        # self.tetromino_z = Tetromino(self.screen, TETROMINO_START_X, TETROMINO_START_Y + TETROMINO_SIZE, 'Z')
+        
         
     def handle_events(self):
         pass
@@ -25,4 +34,14 @@ class Bricktetris:
         self.screen.blit(self.main_screen_image, (MAIN_SCREEN_X, MAIN_SCREEN_Y))
         self.next_screen.draw()
         self.info_screen.draw()
+        self.tetromino_t.draw()
+        self.tetromino_i.draw()
+        self.tetromino_o.draw()
+        self.tetromino_z.draw()
+        self.tetromino_j.draw()
+        self.tetromino_s.draw()
+        self.tetromino_l.draw()
+        # self.tetromino_t.draw()
+        # self.tetromino_o.draw()
+
         pygame.display.flip()
