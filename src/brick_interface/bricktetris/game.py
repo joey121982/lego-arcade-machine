bd854665333af22c244ee460a914f689c00f86d3
cosminbrn <cosminbaroana06@gmail.com>
2025-10-08 17:53:47 +0300

    Improved sprite, added screen classes

--- src/brick_interface/bricktetris/game.py
+++ src/brick_interface/bricktetris/game.py
@@ -1,5 +1,6 @@
 import pygame
 from .constants import *
+from .screens import *
 
 class Bricktetris:
     name = "Brick Tetris"
@@ -12,12 +13,16 @@ class Bricktetris:
         # this will get longer
         self.background_image, self.main_screen_image, self.next_screen_image, self.info_screen_image = load_images()
         
+        # screens
+        self.info_screen = InfoScreen(self.screen, INFO_SCREEN_X, INFO_SCREEN_Y, self.info_screen_image, 0, 1, 0)
+        self.next_screen = NextScreen(self.screen, NEXT_SCREEN_X, NEXT_SCREEN_Y, self.next_screen_image, None)
+        
     def handle_events(self):
         pass
 
     def update(self):
         self.screen.blit(self.background_image, (0, 0))
         self.screen.blit(self.main_screen_image, (MAIN_SCREEN_X, MAIN_SCREEN_Y))
-        self.screen.blit(self.next_screen_image, (NEXT_SCREEN_X, NEXT_SCREEN_Y))
-        self.screen.blit(self.info_screen_image, (INFO_SCREEN_X, INFO_SCREEN_Y))
+        self.next_screen.draw()
+        self.info_screen.draw()
         pygame.display.flip()
--- src/brick_interface/bricktetris/screens.py
+++ src/brick_interface/bricktetris/screens.py
@@ -1,11 +1,13 @@
 import pygame
+from .constants import *
 
 class Screen:
-    def __init__(self, screen, x, y, image)
+    def __init__(self, screen, x, y, image):
         self.screen = screen
         self.x = x
         self.y = y
         self.image = image
+        self.font_text = pygame.font.Font("./assets/bricktetris/fonts/Pixellettersfull-BnJ5.ttf", 36)
 
     def draw(self):
         self.screen.blit(self.image, (self.x, self.y))
@@ -13,6 +15,18 @@ class Screen:
 class PlayableScreen(Screen):
     def __init__(self, screen, x, y, image):
         super().__init__(screen, x, y, image)
+        
+class NextScreen(Screen):
+    def __init__(self, screen, x, y, image, next_piece):
+        super().__init__(screen, x, y, image)
+        self.next_piece = next_piece
+        self.font_text = pygame.font.Font("./assets/bricktetris/fonts/Pixellettersfull-BnJ5.ttf", 54)
+
+    def draw(self):
+        super().draw()
+        # Draw the text "Next" on screen
+        next_text = self.font_text.render("Next", True, WHITE)
+        self.screen.blit(next_text, (self.x + TEXT_X_OFFSET, self.y + TEXT_Y_OFFSET))
 
 class InfoScreen(Screen):
     def __init__(self, screen, x, y, image, score, level, lines):
@@ -24,5 +38,15 @@ class InfoScreen(Screen):
     def draw(self):
         super().draw()
         # Draw score, level, lines on the screen
-        
-    
+        score_text = self.font_text.render(f"Score", True, WHITE)
+        score_info = self.font_text.render(f"{self.score}", True, WHITE)
+        level_text = self.font_text.render(f"Level", True, WHITE)
+        level_info = self.font_text.render(f"{self.level}", True, WHITE)
+        lines_text = self.font_text.render(f"Lines", True, WHITE)
+        lines_info = self.font_text.render(f"{self.lines}", True, WHITE)
+        self.screen.blit(score_text, (self.x + TOP_TEXT_X_OFFSET, self.y + TOP_TEXT_Y_OFFSET))
+        self.screen.blit(score_info, (self.x + TOP_INFO_X_OFFSET, self.y + TOP_INFO_Y_OFFSET))
+        self.screen.blit(level_text, (self.x + MIDDLE_TEXT_X_OFFSET, self.y + MIDDLE_TEXT_Y_OFFSET))
+        self.screen.blit(level_info, (self.x + MIDDLE_INFO_X_OFFSET, self.y + MIDDLE_INFO_Y_OFFSET))
+        self.screen.blit(lines_text, (self.x + BOTTOM_TEXT_X_OFFSET, self.y + BOTTOM_TEXT_Y_OFFSET))
+        self.screen.blit(lines_info, (self.x + BOTTOM_INFO_X_OFFSET, self.y + BOTTOM_INFO_Y_OFFSET))
