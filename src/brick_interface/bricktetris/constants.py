119412f21cc119e1afd062d4a013be7a2db7e9ac
cosminbrn <cosminbaroana06@gmail.com>
2025-10-18 20:53:41 +0300

    Upgraded assets, almost finished tetris

--- src/brick_interface/bricktetris/constants.py
+++ src/brick_interface/bricktetris/constants.py
@@ -1,4 +1,5 @@
 import pygame
+import collections # deque
 
 def load_images():
     background_image = pygame.image.load('./assets/bricktetris/images/tetris_background.jpg').convert()
@@ -7,47 +8,32 @@ def load_images():
     info_screen_image = pygame.image.load('./assets/bricktetris/images/info_screen.png').convert_alpha()
     return background_image, main_screen_image, next_screen_image, info_screen_image
 
-SHAPES = {
-    'I': [[1, 1, 1, 1]],
-    'O': [[1, 1],
-          [1, 1]],
-    'T': [[0, 1, 0],
-          [1, 1, 1]],
-    'S': [[0, 1, 1],
-          [1, 1, 0]],
-    'Z': [[1, 1, 0],
-          [0, 1, 1]],
-    'J': [[0, 0, 1],
-          [1, 1, 1]],
-    'L': [[1, 0, 0],
-          [1, 1, 1]]
+TETROMINOES = {
+    'I': {'shape': [[1, 1, 1, 1]], 'id': 1},
+    'O': {'shape': [[2, 2], [2, 2]], 'id': 2},
+    'T': {'shape': [[0, 3, 0], [3, 3, 3]], 'id': 3},
+    'S': {'shape': [[0, 4, 4], [4, 4, 0]], 'id': 4},
+    'Z': {'shape': [[5, 5, 0], [0, 5, 5]], 'id': 5},
+    'J': {'shape': [[6, 0, 0], [6, 6, 6]], 'id': 6},
+    'L': {'shape': [[0, 0, 7], [7, 7, 7]], 'id': 7}
 }
 
 # colors
 GRAY = (30, 30, 30)
-
-LIGHT_BLUE = (137, 207, 240)
-DARK_BLUE = (0, 0, 255)
 WHITE = (255, 255, 255)
-CYAN = (0, 255, 255)
-YELLOW = (255, 255, 0)
-PURPLE = (128, 0, 128)
-GREEN = (0, 255, 0)
-RED = (255, 0, 0)
-BLUE = (0, 0, 255)
-ORANGE = (255, 165, 0)
 
+# enum type dict
 SHAPE_COLORS = {
-    'I': CYAN ,
-    'O': YELLOW,
-    'T': PURPLE,
-    'S': GREEN,
-    'Z': RED,
-    'J': BLUE,
-    'L': ORANGE
+    1: (0, 255, 255),      # Cyan for 'I'
+    2: (255, 255, 0),      # Yellow for 'O'
+    3: (160, 0, 255),      # Purple for 'T'
+    4: (0, 255, 0),        # Green for 'S'
+    5: (255, 0, 0),        # Red for 'Z'
+    6: (0, 0, 255),        # Blue for 'J'
+    7: (255, 165, 0)       # Orange for 'L'
 }
 
-# screen positioning
+# --- screen positioning ---
 SCREEN_WIDTH = 1920
 SCREEN_HEIGHT = 1080
 LEFT_RIGHT_MARGIN = 150
@@ -68,8 +54,9 @@ NEXT_SCREEN_HEIGHT = INFO_SCREEN_HEIGHT
 MAIN_SCREEN_X = SCREEN_WIDTH // 3
 INFO_SCREEN_X = LEFT_RIGHT_MARGIN
 NEXT_SCREEN_X = SCREEN_WIDTH - LEFT_RIGHT_MARGIN - NEXT_SCREEN_WIDTH
+MAIN_SCREEN_SIDE_MARGIN = 70
 
-# info screen positioning
+# --- info screen text positioning ---
 TOP_TEXT_Y_OFFSET = 25
 TOP_TEXT_X_OFFSET = INFO_SCREEN_WIDTH // 2 - 40
 TOP_INFO_Y_OFFSET = 80
@@ -83,13 +70,21 @@ BOTTOM_TEXT_X_OFFSET = INFO_SCREEN_WIDTH // 2 - 40
 BOTTOM_INFO_Y_OFFSET = 340
 BOTTOM_INFO_X_OFFSET = INFO_SCREEN_WIDTH // 2 - 10
 
-# next screen positioning
+# --- next screen text positioning ---
 TEXT_Y_OFFSET = 37
 TEXT_X_OFFSET = NEXT_SCREEN_WIDTH // 2 - 40
 
-# tetromino
+# --- tetromino && grid ---
+ROWS = 20
+COLS = 10
+BLOCK_SIZE = 48 
+LINE_THICKNESS = 4
 TS = 50 # TETROMINO_SIZE
 TBS = 4 # TETROMINO_BORDER_SIZE
 TBSH = TBS // 2 # TETROMINO_BORDER_SIZE_HALF
 TETROMINO_START_Y = MAIN_SCREEN_TOP_DOWN_MARGIN + MAIN_SCREEN_TOP_DOWN_BORDER_SIZE
-TETROMINO_START_X = MAIN_SCREEN_X + 70 + 4 * TS
+TETROMINO_START_X = MAIN_SCREEN_X + MAIN_SCREEN_SIDE_MARGIN + 4 * TS
+
+# --- controls ---
+MOVE_DOWN_DELAY = 5 # ms
+MOVE_SIDEWAYS_DELAY = 5 # ms
--- src/brick_interface/bricktetris/game.py
+++ src/brick_interface/bricktetris/game.py
@@ -1,7 +1,7 @@
 import pygame
 from .constants import *
 from .screens import *
-from .tetromino import Tetromino
+
 class Bricktetris:
     name = "Brick Tetris"
     running = True
@@ -9,39 +9,113 @@ class Bricktetris:
     def __init__(self, screen, globals):
         self.screen = screen
         self.glb = globals
+        self.clock = pygame.time.Clock()
         
-        # this will get longer
         self.background_image, self.main_screen_image, self.next_screen_image, self.info_screen_image = load_images()
         
-        # screens
-        self.info_screen = InfoScreen(self.screen, INFO_SCREEN_X, INFO_SCREEN_Y, self.info_screen_image, 0, 1, 0)
-        self.next_screen = NextScreen(self.screen, NEXT_SCREEN_X, NEXT_SCREEN_Y, self.next_screen_image, None)
-        self.tetromino_t = Tetromino(self.screen, TETROMINO_START_X - 2 * TS, TETROMINO_START_Y + TS, 'T')
-        self.tetromino_i = Tetromino(self.screen, TETROMINO_START_X - 2 * TS, TETROMINO_START_Y + 3 * TS, 'I')
-        self.tetromino_o = Tetromino(self.screen, TETROMINO_START_X - 1 * TS, TETROMINO_START_Y + 3 * TS, 'O')
-        self.tetromino_z = Tetromino(self.screen, TETROMINO_START_X - 1 * TS, TETROMINO_START_Y + TS, 'Z')
-        self.tetromino_j = Tetromino(self.screen, TETROMINO_START_X + 2 * TS, TETROMINO_START_Y + 5 * TS, 'J')
-        self.tetromino_l = Tetromino(self.screen, TETROMINO_START_X + 2 * TS, TETROMINO_START_Y + 15 * TS, 'L')
-        self.tetromino_s = Tetromino(self.screen, TETROMINO_START_X + 2 * TS, TETROMINO_START_Y + 10 * TS, 'S')
-        # self.tetromino_z = Tetromino(self.screen, TETROMINO_START_X, TETROMINO_START_Y + TETROMINO_SIZE, 'Z')
+        # create screen instances
+        self.playable_screen = PlayableScreen(self.screen, MAIN_SCREEN_X, MAIN_SCREEN_Y)
+        self.info_screen = InfoScreen(self.screen, INFO_SCREEN_X, INFO_SCREEN_Y, self.info_screen_image)
+        self.next_screen = NextScreen(self.screen, NEXT_SCREEN_X, NEXT_SCREEN_Y, self.next_screen_image)
         
+        # game timer, add falling events
+        self.current_level = 1
+        self.fall_speed = 500 # ms
+        self.FALL_EVENT = pygame.USEREVENT + 1
+        pygame.time.set_timer(self.FALL_EVENT, self.fall_speed)
         
-    def handle_events(self):
-        pass
+        # move sideways timers
+        self.move_sideways_timer = 0
+        self.move_sideways_delay = MOVE_SIDEWAYS_DELAY # ms
+        
+        # move down timers
+        self.move_down_timer = 0
+        self.move_down_delay = MOVE_DOWN_DELAY # ms
+
+    def handle_event(self, event):
+        if event.type == self.FALL_EVENT:
+            self.playable_screen.drop()
+
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_ESCAPE:
+                self.running = False
+                if hasattr(self.glb, 'return_to_menu'):
+                    self.glb.return_to_menu = True
+                return
+            
+            if event.key == pygame.K_r:
+                self.__init__(self.screen, self.glb)
+                return
+
+            # if the game is over, we ignore gameplay key presses
+            if self.playable_screen.game_over:
+                return
+
+            # gameplay controls
+            # if event.key == pygame.K_a:
+            #     self.playable_screen.move_left()
+            # elif event.key == pygame.K_d:
+            #     self.playable_screen.move_right()
+            # elif event.key == pygame.K_s:
+            #     self.playable_screen.drop()
+            if event.key == pygame.K_w:
+                self.playable_screen.rotate()
 
     def update(self):
+        # hold down key handling
+        keys = pygame.key.get_pressed()
+        if not self.playable_screen.game_over:
+            move_down = keys[pygame.K_s]
+            if move_down:
+                self.move_down_timer += 1
+                if self.move_down_timer > self.move_down_delay:
+                    self.move_down_timer = 0
+                    self.playable_screen.drop()
+            else:
+                self.move_down_timer = 0
+
+            move_left = keys[pygame.K_a]
+            move_right = keys[pygame.K_d]
+            if move_left or move_right:
+                self.move_sideways_timer += 1
+                if self.move_sideways_timer > self.move_sideways_delay:
+                    self.move_sideways_timer = 0
+                    if move_left:
+                        self.playable_screen.move_left()
+                    if move_right:
+                        self.playable_screen.move_right()
+            else:
+                self.move_sideways_timer = 0
+
+        # draw static background elements
         self.screen.blit(self.background_image, (0, 0))
         self.screen.blit(self.main_screen_image, (MAIN_SCREEN_X, MAIN_SCREEN_Y))
-        self.next_screen.draw()
-        self.info_screen.draw()
-        self.tetromino_t.draw()
-        self.tetromino_i.draw()
-        self.tetromino_o.draw()
-        self.tetromino_z.draw()
-        self.tetromino_j.draw()
-        self.tetromino_s.draw()
-        self.tetromino_l.draw()
-        # self.tetromino_t.draw()
-        # self.tetromino_o.draw()
+        
+        # update and draw game state from the playable screen
+        self.playable_screen.draw()
+        
+        # update and draw info/next screens
+        self.info_screen.draw(
+            self.playable_screen.score, 
+            self.playable_screen.level, 
+            self.playable_screen.lines_cleared_total
+        )
+        self.next_screen.draw(self.playable_screen.next_piece_shape)
+        
+        # difficulty adjustment based on level
+        if self.playable_screen.level != self.current_level:
+            self.current_level = self.playable_screen.level
+            new_fall_speed = max(100, 500 - (self.current_level - 1) * 40)
+            self.fall_speed = new_fall_speed
+            pygame.time.set_timer(self.FALL_EVENT, self.fall_speed)
+
+        # basic game over display
+        if self.playable_screen.game_over:
+            font = pygame.font.Font("./assets/bricktetris/fonts/Pixellettersfull-BnJ5.ttf", 100)
+            text = font.render('GAME OVER', True, WHITE)
+            text_rect = text.get_rect(center=(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2))
+            self.screen.blit(text, text_rect)
 
         pygame.display.flip()
+        self.clock.tick(60)
+
